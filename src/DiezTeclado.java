b8f73f48d7cb0c7220c4c67f1d75c053ecf3d16b
Byeol <99853034+ByeolSBA@users.noreply.github.com>
2025-03-06 23:11:30 -0500

    Update DiezTeclado.java

--- src/DiezTeclado.java
+++ src/DiezTeclado.java
@@ -2,22 +2,30 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class DiezTeclado {
     public static void main (String [] args){
-        Scanner scanner = new Scanner (System.in);
-        int [] Matriz = new int [10];
-        for (int i = 0; i < Matriz.length; i++) {
-            System.out.println("Digite un numero positivo: ");
-            int numero = scanner.nextInt();
-            if (numero < 0){
-                System.out.println("Digite un numero positivo valido");
-                i--;
+        int[] numeros = new int[10];
+        Scanner scanner = new Scanner(System.in);
+        
+        int positivos = 0, negativos = 0, ceros = 0;
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Ingrese el número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+        }
+        
+        for (int i = 0; i < 10; i++) {
+            if (numeros[i] > 0) {
+                positivos++;
+            } else if (numeros[i] < 0) {
+                negativos++;
             } else {
-                Matriz [i] = numero;
+                ceros++;
             }
         }
-        int suma = 0;
-        for (int i = 0; i < Matriz.length; i++) {
-            suma += Matriz[i];
-        }
-        System.out.println(suma);
+        
+        System.out.println("\nCantidad de números positivos: " + positivos);
+        System.out.println("Cantidad de números negativos: " + negativos);
+        System.out.println("Cantidad de ceros: " + ceros);
+        
+        scanner.close();
     }
 }
